d88250374410a784a9a54d8d992c3da9b66d667b
ivankrstev <ivankrstev246@gmail.com>
2023-02-10 16:48:04 +0100

    Added MonoSpace Google Font

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Space_Mono } from "@next/font/google";
+
+const inter = Space_Mono({ subsets: ["latin"], weight: "400" });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
