fa727c20516e8fb71350034246fe452c008721d4
ivankrstev <ivankrstev246@gmail.com>
2023-05-15 19:33:45 +0200

    Added framer-motion for animations while switching pages

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,15 @@
 import "@/styles/globals.css";
 import { Space_Mono } from "@next/font/google";
+import { AnimatePresence } from "framer-motion";
 
 const inter = Space_Mono({ subsets: ["latin"], weight: "400" });
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps, router }) {
   return (
-    <main className={inter.className}>
-      <Component {...pageProps} />
-    </main>
+    <div className={inter.className}>
+      <AnimatePresence mode='wait' initial={false} onExitComplete={() => window.scrollTo(0, 0)}>
+        <Component key={router.asPath} {...pageProps} />
+      </AnimatePresence>
+    </div>
   );
 }
