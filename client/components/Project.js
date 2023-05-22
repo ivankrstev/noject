f3bee962fab6630244120a285171319af304558b
ivankrstev <ivankrstev246@gmail.com>
2023-05-22 20:10:20 +0200

    Added project component with styles dummy data

---
+++ client/components/Project.js
@@ -0,0 +1,46 @@
+import styles from "@/styles/Project.module.css";
+import Task from "./Task";
+import { useEffect } from "react";
+import tasksProgressHandler from "@/public/utils/tasksProgressHandler";
+
+export default function Project({ selectProject }) {
+  useEffect(() => {
+    tasksProgressHandler();
+  }, []);
+
+  return (
+    <div className={styles.projectMain}>
+      <div className={styles.projectHeaderDiv}>
+        <h2>{selectProject}</h2>
+        <span id='projectProgressSpan' title='Project progress'></span>
+      </div>
+      <div
+        id='projectTasksWrapperDiv'
+        className={[styles.projectTasksWrapper, styles.noWrapLines].join(" ")}>
+        <Task
+          completed={true}
+          levelProp={0}
+          text='asdasdasdasd changing info This is a task for
+        changing info This is a task for changing info'
+        />
+        <Task
+          completed={true}
+          levelProp={0}
+          text='This is a task for changing info This is a task for changing info This is a task for
+        changing info This is a task for changing info This is a task for changing info This is a
+        task for changing info This is a task for changing info This is a task for changing info
+        This is a task for changing info This is a task for'
+        />
+        <Task levelProp={0} text='Main task' />
+        <Task levelProp={1} text='Sub task ' />
+        <Task levelProp={2} text='Sub sub task ' />
+        <Task levelProp={3} text='Sub sub sub task ' />
+        <Task completed={true} levelProp={3} text='Sub sub sub task ' />
+        <Task completed={true} levelProp={3} text='Sub sub sub task ' />
+        <Task completed={true} levelProp={2} text='Sub sub task ' />
+        <Task levelProp={1} text='Sub task' />
+        <Task levelProp={0} text='Main Task' />
+      </div>
+    </div>
+  );
+}
