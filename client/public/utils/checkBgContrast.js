8160ec84495ed8def05b846da6e5034d72bd52fd
ivankrstev <ivankrstev246@gmail.com>
2023-05-22 19:49:42 +0200

    Deleted the contrast checker and color generator

--- client/public/utils/checkBgContrast.js
+++
@@ -1,7 +0,0 @@
-export default function (rgbColor) {
-  rgbColor = rgbColor.replace("rgb(", "");
-  rgbColor = rgbColor.replace(")", "");
-  const rgb = rgbColor.split(", ");
-  const yiq = (rgb[0] * 299 + rgb[1] * 587 + rgb[2] * 114) / 1000;
-  return yiq >= 128 ? "#000" : "#FFF";
-}
