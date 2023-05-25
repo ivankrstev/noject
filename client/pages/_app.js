a345034e0da83e459d06b1bd88a70fe9b7f864f6
ivankrstev <ivankrstev246@gmail.com>
2023-05-25 18:41:08 +0200

    Added ToastContainer for notifications in the app root

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,26 @@
 import "@/styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
 import { Space_Mono } from "@next/font/google";
 import { AnimatePresence } from "framer-motion";
+import { ToastContainer } from "react-toastify";
 
 const inter = Space_Mono({ subsets: ["latin"], weight: "400" });
 
 export default function App({ Component, pageProps, router }) {
   return (
     <div className={inter.className}>
+      <ToastContainer
+        position='top-right'
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='colored'
+      />
       <AnimatePresence mode='wait' initial={false} onExitComplete={() => window.scrollTo(0, 0)}>
         <Component key={router.asPath} {...pageProps} />
       </AnimatePresence>
