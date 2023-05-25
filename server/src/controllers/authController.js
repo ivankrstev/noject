e1478a79c946adf0364b43acfb1ee967d306d64d
ivankrstev <ivankrstev246@gmail.com>
2023-05-25 18:38:51 +0200

    Changed response messages of all auth controllers

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -10,21 +10,21 @@ export const registerUser = async (req, res) => {
   try {
     const value = await newUserSchema.validateAsync(req.body);
     const [rows] = await db.execute("SELECT u_id FROM users WHERE u_id = ?", [value.email]);
-    if (rows.length !== 0) return res.status(409).json({ error: "User already exists" });
+    if (rows.length !== 0) return res.status(409).json({ error: "Email already in use" });
     const hashedPassword = await bcrypt.hash(value.password, 10);
     const tfa_code = speakeasy.generateSecret().base32;
     const [result] = await db.execute(
       "INSERT INTO USERS (u_id, first_name, last_name, password, tfa_code) VALUES (?,?,?,?,?)",
       [value.email, value.firstName, value.lastName, hashedPassword, tfa_code]
     );
-    res.status(201).json({ message: "User created" });
+    res.status(201).json({ message: "Registration successful" });
   } catch (error) {
     if (error instanceof Joi.ValidationError)
       return res
         .status(400)
         .json({ error: capitalizeFirstLetter(error.details[0].message.replaceAll(`"`, ``)) });
     console.log("Error:", error);
-    return res.status(500).json({ error: "A server error occured" });
+    return res.status(500).json({ error: "Oops! Something went wrong" });
   }
 };
 
@@ -60,7 +60,7 @@ export const loginUser = async (req, res) => {
       })
       .json({ accessToken: accessToken });
   } catch (error) {
-    return res.status(500).json({ error: "A server error occured" });
+    return res.status(500).json({ error: "Oops! Something went wrong" });
   }
 };
 
@@ -73,7 +73,7 @@ export const verifyTFA = async (req, res) => {
     const u_id = userId;
     const [rows] = await db.execute("SELECT tfa_code FROM users WHERE u_id = ?", [u_id]);
     const tfa_code = rows[0].tfa_code;
-    if (!tfa_code) return res.status(400).json({ error: "Invalid verifying" });
+    if (!tfa_code) return res.status(400).json({ error: "Oops! Something went wrong" });
     const verified = speakeasy.totp.verify({
       secret: tfa_code,
       encoding: "base32",
@@ -103,7 +103,7 @@ export const verifyTFA = async (req, res) => {
   } catch (error) {
     if (error instanceof jwt.TokenExpiredError)
       return res.status(401).json({ error: "Invalid token" });
-    return res.status(500).json({ error: "A server error occured" });
+    return res.status(500).json({ error: "Oops! Something went wrong" });
   }
 };
 
@@ -114,7 +114,7 @@ export const enableTFA = async (req, res) => {
     const user = req.user;
     const [rows] = db.execute("SELECT tfa_code, tfa_activated FROM users WHERE u_id = ?", [user]);
     const tfaData = rows[0];
-    if (!tfaData) return res.status(500).json({ error: "A server error occured" });
+    if (!tfaData) return res.status(500).json({ error: "Oops! Something went wrong" });
     if (!parseInt(tfaData.tfa_activated))
       return res.status(400).json({ error: "Two Factor Authentication is already enabled" });
     const verified = speakeasy.totp.verify({
@@ -135,7 +135,7 @@ export const disableTFA = async (req, res) => {
     const user = req.user;
     const [rows] = db.execute("SELECT tfa_code, tfa_activated FROM users WHERE u_id = ?", [user]);
     const tfaData = rows[0];
-    if (!tfaData) return res.status(500).json({ error: "A server error occured" });
+    if (!tfaData) return res.status(500).json({ error: "Oops! Something went wrong" });
     if (!parseInt(tfaData.tfa_activated))
       return res.status(400).json({ error: "Two Factor Authentication is already disabled" });
     const verified = speakeasy.totp.verify({
@@ -147,7 +147,7 @@ export const disableTFA = async (req, res) => {
     await db.execute("UPDATE users SET tfa_activated = 0, tfa_code = NULL WHERE u_id = ?", [user]);
     return res.status(200).json({ message: "Two Factor Authenitication disabled" });
   } catch (error) {
-    return res.status(500).json({ error: "A server error occured" });
+    return res.status(500).json({ error: "Oops! Something went wrong" });
   }
 };
 
@@ -156,14 +156,14 @@ export const generateTfaSecret = async (req, res) => {
     const user = req.user;
     const [rows] = db.execute("SELECT tfa_activated FROM users WHERE u_id = ?", [user]);
     const tfaData = rows[0];
-    if (!tfaData) return res.status(500).json({ error: "A server error occured" });
+    if (!tfaData) return res.status(500).json({ error: "Oops! Something went wrong" });
     if (parseInt(tfaData.tfa_activated))
       return res.status(400).json({ error: "Two Factor Authentication is already enabled" });
     const tfa_code = speakeasy.generateSecret().base32;
     await db.execute("UPDATE users SET tfa_code = NULL WHERE u_id = ?", [tfa_code, user]);
     return res.status(201).json({ secret: tfa_code });
   } catch (error) {
-    return res.status(500).json({ error: "A server error occured" });
+    return res.status(500).json({ error: "Oops! Something went wrong" });
   }
 };
 
@@ -184,7 +184,7 @@ export const refreshToken = async (req, res) => {
   } catch (error) {
     if (error instanceof jwt.TokenExpiredError)
       return res.status(401).json({ error: "Token is invalid or expired" });
-    return res.status(500).json({ error: "A server error occured" });
+    return res.status(500).json({ error: "Oops! Something went wrong" });
   }
 };
 
@@ -199,6 +199,6 @@ export const signOut = async (req, res) => {
     ]);
     return res.clearCookie("refreshToken").redirect("/login");
   } catch (error) {
-    return res.status(500).json({ error: "A server error occured" });
+    return res.status(500).json({ error: "Oops! Something went wrong" });
   }
 };
