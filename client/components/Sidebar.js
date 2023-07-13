11cae529e674e137068ebacfbdc53232f9b1be69
ivankrstev <ivankrstev246@gmail.com>
2023-07-13 11:54:00 +0200

    Added button for new project and customized project items

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -1,8 +1,12 @@
 import styles from "@/styles/Sidebar.module.css";
 import closeMenuIcon from "@/public/icons/close-menu.svg";
+import addIcon from "@/public/icons/add_plus.svg";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Sidebar(props) {
+  const [showNewProjectModal, setShowNewProjectModal] = useState(false);
+
   return (
     <div className={[styles.sidebar, !props.showSidebar && styles.sidebarHide].join(" ")}>
       <div className={styles.headerWrapper}>
@@ -16,6 +20,10 @@ export default function Sidebar(props) {
       </div>
       <div className={styles.myProjHeader}>
         <h5>My projects</h5>
+        <button className={styles.newProjectBtn} onClick={() => setShowNewProjectModal(true)}>
+          <Image width={20} src={addIcon} alt='New icon' />
+          Create Project
+        </button>
       </div>
       <div id='p1' className={styles.sidebarProjectItem} title='Weather Report for the cast'>
         <div className={styles.sidebarProjectSquare}>W</div>
