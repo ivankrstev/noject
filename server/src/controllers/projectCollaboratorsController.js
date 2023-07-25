5f6ad08f0cda99ff76e37050c39e8bc2d8a13b8d
ivankrstev <ivankrstev246@gmail.com>
2023-07-26 01:16:44 +0200

    Added 'get collaborators' controller and mapped to /project-controllers/:p_id

--- server/src/controllers/projectCollaboratorsController.js
+++ server/src/controllers/projectCollaboratorsController.js
@@ -27,3 +27,15 @@ export const addCollaborator = async (req, res) => {
     return res.status(500).json({ error: "Oops! Something went wrong" });
   }
 };
+
+export const getCollaborators = async (req, res) => {
+  try {
+    const { p_id } = req.params;
+    const [rows] = await db.execute("SELECT u_id FROM project_collaborators WHERE p_id = ?", [
+      p_id,
+    ]);
+    return res.status(200).json(rows);
+  } catch (error) {
+    return res.status(500).json({ error: "Oops! Something went wrong" });
+  }
+};
--- server/src/routes/projectCollaboratorRoute.js
+++ server/src/routes/projectCollaboratorRoute.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import authenticateUser from "../middlewares/authenticateUser.js";
-import { searchUsers, addCollaborator } from "../controllers/projectCollaboratorsController.js";
+import {
+  searchUsers,
+  addCollaborator,
+  getCollaborators,
+} from "../controllers/projectCollaboratorsController.js";
 import verifyProjectOwnership from "../middlewares/verifyProjectOwnership.js";
 
 const router = Router();
 
 router.get("/search", authenticateUser, searchUsers);
 router.post("/add", authenticateUser, verifyProjectOwnership, addCollaborator);
+router.get("/:p_id", authenticateUser, verifyProjectOwnership, getCollaborators);
 
 export default router;
