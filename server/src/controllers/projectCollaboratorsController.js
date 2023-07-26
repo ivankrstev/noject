dcffe70eed58331c094e05cbe8644d934d603b7a
ivankrstev <ivankrstev246@gmail.com>
2023-07-26 03:01:10 +0200

    Added 404 responses if there is a parameter, body or query element missing

--- server/src/controllers/projectCollaboratorsController.js
+++ server/src/controllers/projectCollaboratorsController.js
@@ -3,10 +3,10 @@ import db from "../../db/index.js";
 export const searchUsers = async (req, res) => {
   try {
     const { q } = req.query;
+    if (!q || q === "") return res.status(400).json({ error: "Query(q) is missing" });
     const [rows] = await db.execute(`SELECT u_id FROM users WHERE u_id LIKE CONCAT(? , "%")`, [q]);
     return res.status(200).json(rows);
   } catch (error) {
-    console.error(error);
     return res.status(500).json({ error: "Oops! Something went wrong" });
   }
 };
@@ -14,6 +14,8 @@ export const searchUsers = async (req, res) => {
 export const addCollaborator = async (req, res) => {
   try {
     const { userToAdd, p_id } = req.body;
+    if (!userToAdd || userToAdd === "")
+      return res.status(400).json({ error: "User Id(userToAdd) is missing" });
     await db.execute("INSERT INTO project_collaborators (p_id, u_id) VALUES (?, ?)", [
       p_id,
       userToAdd,
@@ -44,7 +46,7 @@ export const removeCollaborator = async (req, res) => {
   try {
     const { p_id } = req.params;
     const { u_id } = req.query;
-    if (!u_id || u_id === "") return;
+    if (!u_id || u_id === "") return res.status(400).json({ error: "User Id(u_id) is missing" });
     const rows = await db.execute("DELETE FROM project_collaborators WHERE p_id = ? AND u_id = ?", [
       p_id,
       u_id,
