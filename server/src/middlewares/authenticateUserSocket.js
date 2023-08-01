2b6a257bdb9b7cd93000136ef50cb89aca52d108
ivankrstev <ivankrstev246@gmail.com>
2023-08-01 03:08:35 +0200

    Added user AUTH middleware with refresh token and socket id saving to node in-memory

--- server/src/middlewares/authenticateUserSocket.js
+++ server/src/middlewares/authenticateUserSocket.js
@@ -1,11 +1,13 @@
 import jwt from "jsonwebtoken";
+import socketStore from "../sockets/connectedUsers.js";
 
 export default function authenticateUserSocket(socket, next) {
   try {
-    const token = socket.handshake.auth.token;
+    const token = socket.handshake.cookies.refreshToken;
     if (!token) return next(new Error("Authentication error: Token not provided"));
-    const user = jwt.verify(token, process.env.ACCESS_JWT_SECRET);
+    const user = jwt.verify(token, process.env.REFRESH_JWT_SECRET);
     socket.user = user.userId;
+    socketStore.setSocketId(user.userId, socket.id);
     next();
   } catch (error) {
     console.error(error);
