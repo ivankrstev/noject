c824fb31d303340e22608ff7dd5f52f48db8263f
ivankrstev <ivankrstev246@gmail.com>
2023-08-01 03:09:42 +0200

    Added class for managing socket ids according to user email in node in-memory

--- server/src/sockets/connectedUsers.js
+++ server/src/sockets/connectedUsers.js
@@ -1,4 +1,24 @@
-let connectedUsers = {};
+class SocketStore {
+  constructor() {
+    this.connectedUsers = new Map();
+  }
 
-export const setSocketId = (email, socketId) => (connectedUsers[email] = socketId);
-export const getSocketId = (email) => connectedUsers[email];
+  setSocketId = (email, socketId) => {
+    if (!this.connectedUsers.has(email)) this.connectedUsers.set(email, []);
+    this.connectedUsers.get(email).push(socketId);
+  };
+
+  getSocketIds = (email) => this.connectedUsers.get(email) || [];
+
+  removeSocketByEmail = (email, socketId) => {
+    const socketIds = this.connectedUsers.get(email);
+    if (socketIds) {
+      const index = socketIds.indexOf(socketId);
+      if (index !== -1) socketIds.splice(index, 1);
+    }
+  };
+}
+
+const socketStore = new SocketStore();
+Object.freeze(socketStore);
+export default socketStore;
