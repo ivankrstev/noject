3161047cca01c93757d82f47ddf94cc574d5ef3b
ivankrstev <ivankrstev246@gmail.com>
2023-08-08 06:10:51 +0200

    Added tasks fetching with selecting project from sidebar and rendering with custom scrollbars wrapper

--- client/components/Project.js
+++ client/components/Project.js
@@ -1,46 +1,75 @@
 import styles from "@/styles/Project.module.css";
 import Task from "./Task";
-import { useEffect } from "react";
+import { Fragment, useEffect, useState } from "react";
 import tasksProgressHandler from "@/utils/tasksProgressHandler";
+import api from "@/utils/api";
+import { toast } from "react-toastify";
+import { HashLoader } from "react-spinners";
 
-export default function Project({ selectProject }) {
+export default function Project({ selectedProject }) {
+  const [tasks, setTasks] = useState();
+
   useEffect(() => {
-    tasksProgressHandler();
-  }, []);
+    if (tasks && tasks.length !== 0) tasksProgressHandler();
+  }, [tasks]);
+
+  const getProjectTasks = async () => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 400));
+      if (selectedProject) {
+        const response = await api.get("/tasks/" + selectedProject.id);
+        setTasks(response.data);
+      }
+    } catch (error) {
+      toast.error(error.response?.data?.error || error.message || "Error fetching tasks");
+    }
+  };
+  useEffect(() => {
+    setTasks(null);
+    getProjectTasks();
+  }, [selectedProject]);
 
   return (
-    <div id='projectMainDiv' className={styles.projectMain}>
-      <div className={styles.projectHeaderDiv}>
-        <h2>{selectProject}</h2>
-        <span id='projectProgressSpan' title='Project progress'></span>
+    <Fragment>
+      <div id='projectMainDiv' className={styles.projectMain}>
+        {selectedProject && (
+          <div className={styles.projectHeaderDiv}>
+            <h2>{selectedProject.name}</h2>
+            <span id='projectProgressSpan' title='Project progress'></span>
+          </div>
+        )}
+        {tasks && (
+          <Fragment>
+            <div id='projectTasksWrapperDiv' className={styles.projectTasksWrapper}>
+              <Fragment>
+                {tasks.length !== 0 ? (
+                  tasks.map((e) => (
+                    <Task
+                      key={e.t_id}
+                      levelProp={e.level}
+                      completed={e.completed}
+                      value={e.value}
+                    />
+                  ))
+                ) : (
+                  <p>No tasks</p>
+                )}
+              </Fragment>
+            </div>
+          </Fragment>
+        )}
       </div>
-      <div
-        id='projectTasksWrapperDiv'
-        className={[styles.projectTasksWrapper, styles.noWrapLines].join(" ")}>
-        <Task
-          completed={true}
-          levelProp={0}
-          text='asdasdasdasd changing info This is a task for
-        changing info This is a task for changing info'
-        />
-        <Task
-          completed={true}
-          levelProp={0}
-          text='This is a task for changing info This is a task for changing info This is a task for
-        changing info This is a task for changing info This is a task for changing info This is a
-        task for changing info This is a task for changing info This is a task for changing info
-        This is a task for changing info This is a task for'
-        />
-        <Task levelProp={0} text='Main task' />
-        <Task levelProp={1} text='Sub task ' />
-        <Task levelProp={2} text='Sub sub task ' />
-        <Task levelProp={3} text='Sub sub sub task ' />
-        <Task completed={true} levelProp={3} text='Sub sub sub task ' />
-        <Task completed={true} levelProp={3} text='Sub sub sub task ' />
-        <Task completed={true} levelProp={2} text='Sub sub task ' />
-        <Task levelProp={1} text='Sub task' />
-        <Task levelProp={0} text='Main Task' />
-      </div>
-    </div>
+      {!tasks && selectedProject && (
+        <div className='center' style={{ transform: "translateY(100%)" }}>
+          <HashLoader size={120} color='#0570eb' />
+        </div>
+      )}
+      {!selectedProject && (
+        <div style={{ marginTop: "6em", textAlign: "center" }}>
+          <p>You have not selected a project.</p>
+          {/* <span style={{ fontSize: "0.9em" }}>Select one or create a new one</span> */}
+        </div>
+      )}
+    </Fragment>
   );
 }
