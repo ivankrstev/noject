0e7263fc9fa3179c36d74561df28c9c88545e6cd
ivankrstev <ivankrstev246@gmail.com>
2023-08-10 01:23:47 +0200

    Added controller for updating a task value and mapped it to /tasks/:p_id-:t_id/value

--- server/src/controllers/tasksController.js
+++ server/src/controllers/tasksController.js
@@ -10,3 +10,20 @@ export const getAllTasks = async (req, res) => {
     return res.status(500).json({ error: "Oops! Something went wrong" });
   }
 };
+
+export const updateTaskValue = async (req, res) => {
+  try {
+    const { p_id, t_id } = req.params;
+    const { value } = req.body;
+    if (!p_id || p_id === "") return res.status(400).json({ error: "value is missing" });
+    await db.execute("UPDATE tasks SET value = ? WHERE p_id = ? AND t_id = ?", [value, p_id, t_id]);
+    req.app
+      .get("io")
+      .to("p-" + p_id)
+      .emit("tasks:textChanged", { t_id, value });
+    return res.status(200).json({ message: "Task value updated" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Oops! Something went wrong" });
+  }
+};
--- server/src/routes/tasksRoute.js
+++ server/src/routes/tasksRoute.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import authenticateUser from "../middlewares/authenticateUser.js";
-import { getAllTasks } from "../controllers/tasksController.js";
+import { getAllTasks, updateTaskValue } from "../controllers/tasksController.js";
 import verifyProjectAccess from "../middlewares/verifyProjectAccess.js";
+import verifyTaskAccess from "../middlewares/verifyTaskAccess.js";
 
 const router = Router();
 
 router.get("/:p_id", authenticateUser, verifyProjectAccess, getAllTasks);
+router.put(
+  "/:p_id-:t_id/value",
+  authenticateUser,
+  verifyProjectAccess,
+  verifyTaskAccess,
+  updateTaskValue
+);
 
 export default router;
