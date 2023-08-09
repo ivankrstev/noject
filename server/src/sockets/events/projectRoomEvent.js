c0957b7197d95114bc51b64c9d1a0e683195b8ae
ivankrstev <ivankrstev246@gmail.com>
2023-08-09 20:09:59 +0200

    Added project access verifying(must be owner or collaborator) on joining projectRoom

--- server/src/sockets/events/projectRoomEvent.js
+++ server/src/sockets/events/projectRoomEvent.js
@@ -1,6 +1,20 @@
+import db from "../../../db/index.js";
+
 export default function projectRoomEvent(socket) {
-  socket.on("projectRoom:join", (data) => {
-    if (data.p_id) socket.join("p-" + data.p_id);
+  socket.on("projectRoom:join", async (data, callback) => {
+    try {
+      const { p_id } = data;
+      if (!p_id || p_id === "") return callback({ error: "Project id(p_id) missing" });
+      const [rows] = await db.execute(
+        "SELECT EXISTS(SELECT p_id FROM projects WHERE created_by = ? AND p_id = ?) OR EXISTS(SELECT p_id FROM project_collaborators WHERE u_id = ? AND p_id = ?) as allowed",
+        [socket.user, p_id, socket.user, p_id]
+      );
+      if (rows.length === 0 || !rows[0].allowed)
+        return callback({ error: "Access Denied. Unauthorized!" });
+      if (data.p_id) socket.join("p-" + data.p_id);
+    } catch (error) {
+      callback({ error: "Oops! Something went wrong" });
+    }
   });
   socket.on("projectRoom:leave", (data) => {
     if (data.p_id) socket.leave("p-" + data.p_id);
