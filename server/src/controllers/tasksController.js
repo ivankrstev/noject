482348ec949806aa8efd5b00442b78db98e38cda
ivankrstev <ivankrstev246@gmail.com>
2023-08-11 22:02:53 +0200

    Added query for getting first_task value and changed orderTasks to use first_task variable

--- server/src/controllers/tasksController.js
+++ server/src/controllers/tasksController.js
@@ -1,11 +1,20 @@
 import db from "../../db/index.js";
 import socketStore from "../sockets/connectedUsers.js";
+import orderTasks from "../utils/orderTasks.js";
 
 export const getAllTasks = async (req, res) => {
   try {
     let { p_id } = req.params;
     const [rows] = await db.execute("SELECT * FROM tasks WHERE p_id = ?", [p_id]);
-    return res.status(200).json(rows);
+    const [[{ first_task }]] = await db.execute("SELECT first_task FROM projects WHERE p_id = ?", [
+      p_id,
+    ]);
+    // Check if there are tasks, but no first_task pointer. Should not happen
+    if (!first_task && rows.length !== 0)
+      return res.status(500).json({ error: "Oops! Something went wrong" });
+    if (!first_task) return res.status(200).json([]);
+    const tasks = orderTasks(rows, first_task);
+    return res.status(200).json(tasks);
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: "Oops! Something went wrong" });
@@ -24,6 +33,14 @@ export const updateTaskValue = async (req, res) => {
       .except(socketStore.getSocketIds(req.user)) // Except all socket ids that belong to the user who updated the task
       .emit("tasks:value-changed", { t_id, value });
     return res.status(200).json({ message: "Task value updated" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Oops! Something went wrong" });
+  }
+};
+
+export const updateTasksLevels = async (req, res) => {
+  try {
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: "Oops! Something went wrong" });
--- server/src/utils/orderTasks.js
+++ server/src/utils/orderTasks.js
@@ -1,26 +1,23 @@
 // Function to order tasks based on prev_id, next_id and to convert them from array to object
-export default function orderTasks(tasksArray) {
-  let counter = 0;
-  if (tasksArray.length === 1) return [...tasksArray[0]];
+export default function orderTasks(tasksArray, first_task) {
+  if (tasksArray.length === 0) return [];
+  if (tasksArray.length === 1) return tasksArray;
   let current_next = null;
-  let temp = [];
+  let orderedArray = [];
   for (let i = 0; i < tasksArray.length; i++) {
-    counter++;
-    const { t_id, next, prev } = tasksArray[i];
-    const taskNode = {
-      ...tasksArray[i],
-    };
-    // delete taskNode.prev;
-    if (!prev && temp.length === 0) {
-      temp.push(taskNode);
+    const { t_id, next } = tasksArray[i];
+    const taskNode = tasksArray[i];
+    delete taskNode.p_id;
+    if (orderedArray.length === 0 && t_id === first_task) {
+      orderedArray.push(taskNode);
       current_next = next;
       i = -1;
     } else if (current_next === t_id) {
-      temp.push(taskNode);
+      orderedArray.push(taskNode);
       current_next = next; // Set the pointer for the next element to be searched and added to the array
       i = -1;
       if (next === null) break;
     }
   }
-  return temp;
+  return orderedArray;
 }
