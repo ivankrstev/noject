b6298fd50a0f0abb548ecf517b35ab4300aa1c49
ivankrstev <ivankrstev246@gmail.com>
2023-08-19 21:04:41 +0200

    Added comments for more tricky functions in the tasksProgressHandler

--- client/utils/tasks/tasksProgressHandler.js
+++ client/utils/tasks/tasksProgressHandler.js
@@ -33,39 +33,42 @@ const getParentTask = (node) => {
   return null;
 };
 
+// Calculate progress only for a main task
 const calculateMainTask = (mainTask) => {
-  let allSubTasks = [];
-  let nextSibling = mainTask.nextSibling;
-  // Find all subtasks (children, grandchildren, great grandchildren, ...) of the main task
-  while (nextSibling && parseInt(nextSibling.getAttribute("level")) > 0) {
-    allSubTasks.push(nextSibling);
-    nextSibling = nextSibling.nextSibling;
-  }
+  let allSubTasks = getAllSubTasks(mainTask);
   if (allSubTasks.length === 0)
     mainTask.childNodes[0].innerText = mainTask.childNodes[1].checked ? "100%" : "0%";
-  // Find the highest level
+  // Find the maximum level of the siblings of the mainTask
   let maximumLevel = 0;
   allSubTasks.forEach((subtask) => {
     if (parseInt(subtask.getAttribute("level")) > maximumLevel)
       maximumLevel = parseInt(subtask.getAttribute("level"));
   });
   let currentLevel = maximumLevel;
+  // Start traversing the subtasks according to their levels (from maximum to minimum level)
   while (currentLevel >= 0) {
     allSubTasks.forEach((subtask) => {
+      // If the current subtask level is matching the target currentLevel
       if (parseInt(subtask.getAttribute("level")) === currentLevel) {
         if (currentLevel === maximumLevel)
+          // Set the progress of the subtask that has no childs (maximum level)
           subtask.childNodes[0].innerText = subtask.childNodes[1].checked ? "100%" : "0%";
         const parentTask = getParentTask(subtask);
         const parentTaskChilds = getChildTasks(parentTask);
         let completeParent = true;
         let parentPercentages = 0;
+        // Loop through the siblings of the subtask (childs of the parent task of the subtask)
         for (let i = 0; i < parentTaskChilds.length; i++) {
+          // If there is an uncompleted child, set completeParent = false
           !parentTaskChilds[i].childNodes[1].checked && (completeParent = false);
+          // Get the progress from the childs of the parent task and sum them up
           parentPercentages += parseFloat(parentTaskChilds[i].childNodes[0].innerText);
         }
+        // If a task has only one child, then its progress value can be either 100 or 0
         if (parentTaskChilds.length === 1 && parentPercentages !== 100) parentPercentages = 0;
         if (parentTask) {
           parentTask.childNodes[1].checked = completeParent;
+          // Assign the progress to the parent task of the subtask
           parentTask.childNodes[0].innerText =
             parseFloat((parentPercentages / parentTaskChilds.length).toFixed(2)) + "%";
         }
@@ -75,14 +78,19 @@ const calculateMainTask = (mainTask) => {
   }
 };
 
+// Function to assign progress to all the main tasks (of level 0) and calculate the whole project progress
 export default function calculateProject() {
-  // Assign progress to all the main tasks
-  let tasks = Array.from(document.querySelectorAll("#projectTasksWrapperDiv > div"));
-  tasks = tasks.filter((task) => parseInt(task.getAttribute("level")) === 0);
+  let tasks = Array.from(document.querySelectorAll("#projectTasksWrapperDiv > div")); // Get all the tasks
+  tasks = tasks.filter((task) => parseInt(task.getAttribute("level")) === 0); // Filter the main tasks only
+  // For each main task run calculateMainTask to calculate the
+  // progress for the main task and each of its subtasks
   tasks.forEach((task) => calculateMainTask(task));
-  // Calculate the project progress
-  let percentages = 0;
-  tasks.forEach((task) => (percentages += parseFloat(task.childNodes[0].innerText)));
+  // Calculate the whole project progress
+  let projectProgress = 0;
+  // Sum the progress from every main task in the variable percentages
+  tasks.forEach((task) => (projectProgress += parseFloat(task.childNodes[0].innerText)));
+  // Get the span that shows the whole project progress
   const projectProgressSpan = document.querySelector("#projectProgressSpan");
-  projectProgressSpan.innerText = parseFloat((percentages / tasks.length).toFixed(2)) + "%";
+  // Assign the whole project progress to the span
+  projectProgressSpan.innerText = parseFloat((projectProgress / tasks.length).toFixed(2)) + "%";
 }
