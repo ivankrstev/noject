11e3a3f30ce89bddae927eb1b7b5b7010d8eba80
ivankrstev <ivankrstev246@gmail.com>
2023-08-19 22:36:51 +0200

    Changed subtasks fetcher, added decrease level api request function

--- client/utils/tasks/decreaseTaskLevel.js
+++ client/utils/tasks/decreaseTaskLevel.js
@@ -1,18 +1,27 @@
-import tasksProgressHandler from "@/utils/tasks/tasksProgressHandler";
+import tasksProgressHandler, { getAllSubTasks } from "@/utils/tasks/tasksProgressHandler";
+import api from "../api";
 
 export default function decreaseLevel(taskRef) {
   const taskElement = taskRef.current;
+  decreaseLevelApiRequest(taskElement.id);
   const currentLevel = parseInt(taskElement.getAttribute("level"));
-  let temp = taskElement.nextSibling;
-  while (temp && parseInt(temp.getAttribute("level")) > currentLevel) {
-    console.log("temp:", temp);
-    const tempLevel = parseInt(temp.getAttribute("level"));
-    temp.setAttribute("level", tempLevel - 1);
-    temp.style.marginLeft = ((tempLevel - 1) * 1.1).toFixed(1) + "em";
-    temp.childNodes[0].innerText = temp.childNodes[1].checked ? "100%" : "0%";
-    temp = temp.nextSibling;
-  }
+  const subtasks = getAllSubTasks(taskElement);
+  subtasks.forEach((subtask) => {
+    const subtaskLevel = parseInt(subtask.getAttribute("level"));
+    subtask.setAttribute("level", subtaskLevel - 1);
+    subtask.style.marginLeft = ((subtaskLevel - 1) * 1.1).toFixed(1) + "em";
+    subtask.childNodes[0].innerText = subtask.childNodes[1].checked ? "100%" : "0%";
+  });
   taskElement.setAttribute("level", currentLevel - 1);
   taskElement.style.marginLeft = ((currentLevel - 1) * 1.1).toFixed(1) + "em";
   tasksProgressHandler();
 }
+
+export const decreaseLevelApiRequest = async (t_id) => {
+  try {
+    const response = await api.put(`/tasks/${t_id}/decrease-level`);
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+};
