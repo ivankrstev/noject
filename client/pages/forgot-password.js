be7ecbdf63f43e062e71f6053a297705876b2ce7
ivankrstev <ivankrstev246@gmail.com>
2023-08-20 22:40:32 +0200

    Added forgot-password request function

--- client/pages/forgot-password.js
+++ client/pages/forgot-password.js
@@ -1,11 +1,28 @@
 import Head from "next/head";
-import { Fragment, useState } from "react";
+import { Fragment } from "react";
 import styles from "@/styles/SignUpLogIn.module.css";
 import Link from "next/link";
 import Logo from "@/components/Logo";
+import { useRouter } from "next/router";
+import axios from "axios";
+import { toast } from "react-toastify";
+import AxiosErrorHandler from "@/utils/AxiosErrorHandler";
 
-export default function ResetPassword() {
-  const [email, setEmail] = useState("");
+export default function ForgotPassword() {
+  const router = useRouter();
+
+  const postForgotPassword = async (email) => {
+    try {
+      await axios.post(process.env.NEXT_PUBLIC_SERVER_URL + "/forgot-password", {
+        email,
+      });
+      toast.info("Reset link sent. Please check your email inbox");
+      router.push("/login/");
+    } catch (error) {
+      AxiosErrorHandler(error);
+    }
+  };
+
   return (
     <Fragment>
       <Head>
@@ -18,14 +35,16 @@ export default function ResetPassword() {
           <h2>Forgot password?</h2>
           <div className={[styles.inputLabelWrapper, "mt-c1"].join(" ")}>
             <label htmlFor='email'>Email</label>
-            <input
-              placeholder='Enter your email address'
-              type='email'
-              id='email'
-              onChange={(e) => setEmail(e.target.value)}
-            />
+            <input placeholder='Enter your email address' type='email' id='email' />
           </div>
-          <button className={styles.btnSubmit}>Send reset link</button>
+          <button
+            className={styles.btnSubmit}
+            onClick={(e) => {
+              e.preventDefault();
+              postForgotPassword(e.target.form[0].value);
+            }}>
+            Send reset link
+          </button>
           <span className={styles.alterBtns}>
             <Link href='/login'>Log in</Link>
             <span className='colorGrey'>{" or "}</span>
