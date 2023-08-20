609572c398a5e6fb728b0d6047737ee869b78cce
ivankrstev <ivankrstev246@gmail.com>
2023-08-20 22:42:51 +0200

    Changed the email template for sending reset password link

--- server/src/utils/sendResetPwEmail.js
+++ server/src/utils/sendResetPwEmail.js
@@ -11,18 +11,18 @@ const transporter = createTransport({
 });
 
 const sendResetPwEmail = (data) => {
-  const { first_name, last_name, u_id, verification_token } = data;
-  const verificationLink = `${process.env.EMAIL_CLIENT_URL}/reset-password?token=${verification_token}`;
+  const { first_name, last_name, u_id, reset_token } = data;
+  const resetPasswordLink = `${process.env.EMAIL_CLIENT_URL}/reset-password?token=${reset_token}`;
   const mailOptions = {
     from: "Noject App info@noject.live",
     to: `${first_name} ${last_name} ${u_id}`,
     subject: "Password Reset Instructions",
-    text: `Dear ${first_name},\n Please click on the following link to reset your password: ${verificationLink}`,
-    html: `<p style="font-size:16px;color:#333;margin-bottom:10px;">Dear ${first_name},</p><p style="font-size:14px;color:#555;margin-bottom:20px;">Please click on the following link to reset your password:</p><a style="display:inline-block;padding:10px 20px;background-color:#337ab7;color:#fff;text-decoration:none;border-radius:4px;font-size:14px;" href="${verificationLink}">Reset Password</a>`,
+    text: `Dear ${first_name},\n Please click on the following link to reset your password: ${resetPasswordLink}`,
+    html: `<p style="font-size:16px;color:#333;margin-bottom:10px;">Dear ${first_name},</p><p style="font-size:14px;color:#555;margin-bottom:20px;">Please click on the following link to reset your password:</p><a style="display:inline-block;padding:10px 20px;background-color:#337ab7;color:#fff;text-decoration:none;border-radius:4px;font-size:14px;" href="${resetPasswordLink}">Reset Password</a>`,
     priority: "normal",
   };
   transporter.sendMail(mailOptions, (error, info) => {
-    if (error) console.error("Error sending verification email:", error);
+    if (error) console.error("Error sending reset password email:", error);
     console.log(info);
   });
 };
