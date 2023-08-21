decaea3bbe37b362a66f81c71f1822cbfdf425da
ivankrstev <ivankrstev246@gmail.com>
2023-08-21 23:58:52 +0200

    Added api handler for toggling completion of task

--- client/utils/tasks/handleCheckBoxChange.js
+++ client/utils/tasks/handleCheckBoxChange.js
@@ -1,8 +1,11 @@
 import tasksProgressHandler, { getAllSubTasks } from "@/utils/tasks/tasksProgressHandler";
+import api from "../api";
+import AxiosErrorHandler from "../AxiosErrorHandler";
 
 export default function handleCheckBoxChange(event) {
   // Set the percentages for this task if the checkbox is changed
   event.target.previousSibling.innerText = event.target.checked ? "100%" : "0%";
+  toggleTaskCompletion(event.target.parentElement.id);
   // Handle in/completion of all subtasks (children, grandchildren, great grandchildren, ...)
   const allSubTasks = getAllSubTasks(event.target.parentNode);
   allSubTasks.forEach((el) => {
@@ -11,3 +14,11 @@ export default function handleCheckBoxChange(event) {
   });
   tasksProgressHandler();
 }
+
+export const toggleTaskCompletion = async (t_id) => {
+  try {
+    await api.put(`/tasks/${t_id}/toggle-complete`);
+  } catch (error) {
+    AxiosErrorHandler(error);
+  }
+};
