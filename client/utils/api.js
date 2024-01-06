dab81975381cd78c651127989528aaccb3a11608
Ivan Krstev <ivankrstev246@gmail.com>
2024-01-06 23:55:32 +0100

    Added getAccessToken function to api & changed the base url for axios

--- client/utils/api.js
+++ client/utils/api.js
@@ -4,9 +4,26 @@ export const setAccessToken = (token) => {
   accessToken = token;
 };
 
+export const getAccessToken = async () => {
+  if (accessToken) return accessToken;
+  try {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/auth/refresh-token`,
+      {},
+      {
+        withCredentials: true,
+      }
+    );
+    setAccessToken(response.data.accessToken);
+    return response.data.accessToken;
+  } catch (error) {
+    return null;
+  }
+};
+
 // Create api interceptor only for authenticated routes
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:5000",
+  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
 });
 
 let accessToken = "";
@@ -28,7 +45,7 @@ api.interceptors.response.use(
       originalRequest._retry = true;
       try {
         const response = await axios.post(
-          "http://localhost:5000/refresh-token",
+          `${process.env.NEXT_PUBLIC_SERVER_URL}/auth/refresh-token`,
           {},
           {
             withCredentials: true,
