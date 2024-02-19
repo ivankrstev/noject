4fa4d9df588e9b178198bf520ab3ec200ea40607
Ivan Krstev <ivankrstev246@gmail.com>
2024-02-19 23:07:47 +0100

    Changed tasks signal hub to class instance with overriden invoke method

--- client/utils/tasksSignalRHub.js
+++ client/utils/tasksSignalRHub.js
@@ -1,24 +1,36 @@
-import { HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
 import { getAccessToken } from "@/utils/api";
+import { HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
 
-const tasksSocket = new HubConnectionBuilder()
-  .withUrl(`${process.env.NEXT_PUBLIC_SERVER_URL}/TasksHub`, {
-    accessTokenFactory: async () => await getAccessToken(),
-  })
-  .configureLogging(process.env.NEXT_PUBLIC_SIGNALR_LOGGING ? LogLevel.Information : LogLevel.None)
-  .withAutomaticReconnect()
-  .build();
+class TasksSignalRService {
+  #connection;
+  constructor() {
+    this.#connection = new HubConnectionBuilder()
+      .withUrl(`${process.env.NEXT_PUBLIC_SERVER_URL}/TasksHub`, {
+        accessTokenFactory: async () => await getAccessToken(),
+      })
+      .configureLogging(process.env.NEXT_PUBLIC_SIGNALR_LOGGING ? LogLevel.Debug : LogLevel.None)
+      .withAutomaticReconnect()
+      .build();
+  }
 
-async function start() {
-  try {
-    await tasksSocket.start();
-  } catch (error) {}
+  async #startConnection() {
+    try {
+      await this.#connection.start();
+      console.log("signalr connection established");
+    } catch (err) {
+      AxiosErrorHandler(null, router, "Real-time task operations are currently unavailable");
+    }
+  }
+
+  async invoke(methodName, ...args) {
+    try {
+      if (this.#connection.state !== "Connected") await this.#startConnection();
+      return await this.#connection.invoke(methodName, ...args);
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 
-tasksSocket.onclose(async () => {
-  await start();
-});
-
-start();
-
+const tasksSocket = new TasksSignalRService();
 export default tasksSocket;
