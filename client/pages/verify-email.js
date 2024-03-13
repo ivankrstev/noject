933d6901ebdf66fc613e5c0c7953cadd5a9e7065
Ivan Krstev <ivankrstev246@gmail.com>
2024-03-13 20:22:25 +0100

    Changed verify-email api endpoint & page change

--- client/pages/verify-email.js
+++ client/pages/verify-email.js
@@ -1,7 +1,7 @@
-import Head from "next/head";
-import { Fragment, useEffect } from "react";
-import { useRouter } from "next/router";
 import axios from "axios";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { Fragment, useEffect } from "react";
 import { toast } from "react-toastify";
 
 export default function VerifyEmail() {
@@ -11,17 +11,15 @@ export default function VerifyEmail() {
   const verifyToken = async () => {
     try {
       const response = await axios.get(
-        process.env.NEXT_PUBLIC_SERVER_URL + `/verify-email?token=${token}&email=${email}`
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/auth/verify-email?token=${token}&email=${email}`
       );
-      console.log(response);
       toast.success(response.data.message);
-      router.push("/account");
+      router.push("/login");
     } catch (error) {
-      console.error("error:", error);
       if (error.code === "ERR_NETWORK") toast.error("Network Error");
       else if (error.response) toast.error(error.response.data.error);
       else toast.error("Oops! Something went wrong");
-      router.push("/account");
+      router.push("/login");
     }
   };
 
