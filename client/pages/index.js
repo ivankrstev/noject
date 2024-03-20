f0e0cbd7eb5696f234d6a4cf5ab3ffdfdce65c88
Ivan Krstev <ivankrstev246@gmail.com>
2024-03-20 19:20:31 +0100

    Removed motion animation

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,6 @@
 import Head from "next/head";
-import styles from "@/styles/Home.module.css";
 import Link from "next/link";
 import { Fragment } from "react";
-import { motion } from "framer-motion";
 
 export default function Home() {
   return (
@@ -13,16 +11,11 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <motion.div
-        key='homepage'
-        initial={{ opacity: 0, x: -200, y: 0 }}
-        animate={{ opacity: 1, x: 0, y: 0 }}
-        exit={{ opacity: 0, x: 0, y: -100 }}
-        transition={{ type: "linear" }}>
+      <div>
         <Link href={"/login/"}>Log In</Link>
         <Link href={"/signup/"}>Sign Up</Link>
         <Link href={"/dashboard/"}>Dasboard</Link>
-      </motion.div>
+      </div>
     </Fragment>
   );
 }
